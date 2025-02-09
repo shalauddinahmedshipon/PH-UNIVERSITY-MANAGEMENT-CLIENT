99470e9059848613982a462daaca143acd3596e8
MD. SHIPON <sshimi910@gmail.com>
2025-02-08 22:08:00 -0800

    create AuthSlice ,baseApi and  set cookies in the browers

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,43 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 
 const Login = () => {
+  const {register,handleSubmit}=useForm({
+    defaultValues:{
+      userId:"A-0001",
+      password:"123456"
+    }
+  });
+const [login,{data,error}] = useLoginMutation();
+
+console.log({"data":data,
+  "error":error
+});
+  const onSubmit=(data)=>{
+ const userInfo={
+  id:data.userId,
+  password:data.password
+ }
+ login(userInfo);
+ console.log(userInfo);
+  }
+
+
   return (
     <div>
-      login
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text"  id="id" {...register('userId')} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text"  id="password" {...register('password')} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
     </div>
   );
 };
