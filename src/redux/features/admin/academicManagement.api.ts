1d1d7d2b9b353264b457d4aad129fb31e5a69ede
MD. SHIPON <sshimi910@gmail.com>
2025-02-14 20:35:56 -0800

    Handle table filter and redux params

--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,43 +1,43 @@
 
-import { TResponseRedux } from "../../../types";
-import { TAcademicSemester } from "../../../types/academicManagement.type";
-import { baseApi } from "../../api/baseApi";
+// import { TResponseRedux } from "../../../types";
+// import { TAcademicSemester } from "../../../types/academicManagement.type";
+// import { baseApi } from "../../api/baseApi";
 
-const academicManagementApi = baseApi.injectEndpoints(
-  {
-    endpoints:(builder)=>({
-      getAllSemesters:builder.query({
-        query:(args)=>{
-          const params = new URLSearchParams();
-          if(args){
-            args.forEach(item => {
-              params.append(item.name,item.value);
-            });
-          }
+// const academicManagementApi = baseApi.injectEndpoints(
+//   {
+//     endpoints:(builder)=>({
+//       getAllSemesters:builder.query({
+//         query:(args)=>{
+//           const params = new URLSearchParams();
+//           if(args){
+//             args.forEach(item => {
+//               params.append(item.name,item.value);
+//             });
+//           }
          
-          return{
-            url:"/academic-semesters",
-            method:"GET",
-            params:params
-          }
-        },
-        transformResponse:(response:TResponseRedux<TAcademicSemester[]>)=>{
-          return{
-            data:response.data,
-            meta:response?.meta
-          }
-        }
-      }),
+//           return{
+//             url:"/academic-semesters",
+//             method:"GET",
+//             params:params
+//           }
+//         },
+//         transformResponse:(response:TResponseRedux<TAcademicSemester[]>)=>{
+//           return{
+//             data:response.data,
+//             meta:response?.meta
+//           }
+//         }
+//       }),
       
-      addAcademicSemester:builder.mutation({
-        query:(data)=>({
-          url:"/academic-semesters/create-academic-semester",
-          method:"POST",
-          body:data
-        })
-      })
-    })
-  }
-)
+//       addAcademicSemester:builder.mutation({
+//         query:(data)=>({
+//           url:"/academic-semesters/create-academic-semester",
+//           method:"POST",
+//           body:data
+//         })
+//       })
+//     })
+//   }
+// )
 
-export const {useGetAllSemestersQuery,useAddAcademicSemesterMutation}=academicManagementApi;
+// export const {useGetAllSemestersQuery,useAddAcademicSemesterMutation}=academicManagementApi;
