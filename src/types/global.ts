32a009fd532ede0c8e13ee23192423a25577761b
MD. SHIPON <sshimi910@gmail.com>
2025-02-14 21:08:30 -0800

    loading state set in the table and fix type errors

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,4 +1,5 @@
 import { BaseQueryApi } from "@reduxjs/toolkit/query";
+import React from "react";
 
 export type TError={
   data:{
@@ -25,3 +26,8 @@ message:string
 }
 
 export type TResponseRedux<T> = TResponse<T>&BaseQueryApi;
+
+export type TQueryParam={
+  name:string;
+  value:boolean|React.Key;
+}
